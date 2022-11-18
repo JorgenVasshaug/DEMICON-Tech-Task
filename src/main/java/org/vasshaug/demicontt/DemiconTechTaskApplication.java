779ddf4c9ecbd98c628650d468fcf570dd2ab0ad
Jorgen Vasshaug <jorgenv.dev@gmail.com>
2022-11-18 16:11:20 +0100

    Initial Spring Initialization

---
+++ src/main/java/org/vasshaug/demicontt/DemiconTechTaskApplication.java
@@ -0,0 +1,13 @@
+package org.vasshaug.demicontt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemiconTechTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemiconTechTaskApplication.class, args);
+	}
+
+}
