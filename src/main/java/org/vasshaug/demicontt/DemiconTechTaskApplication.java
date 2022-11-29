e4787b03c3310588d48f1027e5840ac4ae22c44e
Jorgen Vasshaug <jorgenv.dev@gmail.com>
2022-11-29 12:56:12 +0100

    Refactored refactoring list ;-) - out into its proper file

--- src/main/java/org/vasshaug/demicontt/DemiconTechTaskApplication.java
+++ src/main/java/org/vasshaug/demicontt/DemiconTechTaskApplication.java
@@ -12,28 +12,4 @@ public class DemiconTechTaskApplication {
 		SpringApplication.run(DemiconTechTaskApplication.class, args);
 	}
 
-	/* @TODO
-
-       Refactoring/Improvements
-        - Make RandomuserAPI static with DI values, or move DI into the RandomUserAPI class and remove need for function variables
-        - If possible remove Results from persistence (remember to update that List<User> are returned instead of Result)
-		- jobtask:  delete previous records before saving into DB
-		- Better handling of possible exceptions throughout code
-		- Move js code in app.js to own classes (components)
-		- Add CSS to frontend
-		- render list using a table
-		- Persist response from randomuser API as String in DB, instead of using domain entities
-		- Rename project to generic name
-
-
-		Considerations:
-		- Is it possible to replace RestController with just the Spring Data Rest dependency ?
-			<dependency>
-				<groupId>org.springframework.boot</groupId>
-				<artifactId>spring-boot-starter-data-rest</artifactId>
-			</dependency>
-        - Use @Data to simplify datamodel - https://projectlombok.org/features/Data
-        - Use @AllArgsConstructor to simplify datamodel - https://projectlombok.org/features/constructor
-     */
-
 }
